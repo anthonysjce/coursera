1ae864c69a87f2c20e7b789fcc73e9b57cec4928
KV <anthony.kv@capgemini.com>
2020-08-01 19:22:46 +0530

    form and validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 import { LoginComponent } from './login/login.component';
-import { MatDialogModule } from '@angular/material';
+import { MatDialogModule, MatFormFieldModule, MatInputModule, MatCheckboxModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -46,7 +47,11 @@ import { MatDialogModule } from '@angular/material';
     MatCardModule,
     MatButtonModule,
     MatDialogModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule
   ],
   providers: [DishService,PromotionService,LeaderService],
   bootstrap: [AppComponent],
