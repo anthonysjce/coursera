5001f39bcbfb77553376fe513cfb5b948af7c1c3
KV <anthony.kv@capgemini.com>
2020-08-03 19:09:29 +0530

    observable retuning promise

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
 import { resolve } from 'url';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,21 +15,24 @@ export class DishService {
    }
    getDishes():Promise<Dish[]>{
      //return Promise.resolve(DISHES);
-     return new Promise(resolve =>{
+    /*  return new Promise(resolve =>{
        setTimeout(() => resolve(DISHES),2000);
-     })
+     }) */
+     return of(DISHES).pipe(delay(2000)).toPromise();// Obeservable returning promise itself
    }
    getDish(id:string): Promise<Dish>{
     //return Promise.resolve(DISHES.filter((dish) => (dish.id === id))[0]);
-    return new Promise(resolve =>{
+   /*  return new Promise(resolve =>{
       setTimeout(()=> resolve(DISHES.filter((dish) => (dish.id === id))[0]),2000);
-    })
+    }) */
+     return of(DISHES.filter((dish) => (dish.id === id))[0]).pipe(delay(2000)).toPromise();
     
   }
   getFeaturedDish(): Promise<Dish>{
-    return  new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
+     // Simulate server latency with 2 second delay
+   /*  return  new Promise(resolve=> {     
         setTimeout(() => resolve(DISHES.filter((dish) => dish.featured)[0]), 2000);
-    });
+    }); */
+    return of(DISHES.filter((dish) => dish.featured)[0]).pipe(delay(2000)).toPromise();
   }
 }
--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
 import { resolve } from 'url';
-import { Lexer } from '@angular/compiler';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,14 +15,16 @@ export class LeaderService {
 
   getLeaders(): Promise<Leader[]>{
     //return Promise.resolve(LEADERS);
-    return new Promise(resolve => {
+   /*  return new Promise(resolve => {
       setTimeout(() => resolve(LEADERS),2000);
-    });
+    }); */
+    return of(LEADERS).pipe(delay(2000)).toPromise();
   }
   getFeaturedLeader(): Promise<Leader> {
     //return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
-    return new Promise(resolve => {
-      setTimeout(()=>resolve(LEADERS.filter((leader) => leader.featured)[0]),2000);
-    })
+   /*  return new Promise(resolve => {
+      setTimeout(()=>resolve(LEADERS.filter((leader) => leader.featured)[0]),2000)
+    }); */
+    return of(LEADERS.filter((leader) => leader.featured)[0]).pipe(delay(2000)).toPromise();
   }
 }
